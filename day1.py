4238e721d4b3e5170b6162b92e7878a98ca08882
Shantanu <shantanuubhe9@gmail.coom>
2025-07-22 17:46:37 +0530

    Upated Day 1

---
+++ day1.py
@@ -0,0 +1,27 @@
+# Q1 -> print 5 statements
+print("Hello World") # print() Function Used To Show Specific 
+print("Hii")
+print("Python is Love")
+print("Hello Python")
+print("Byyy")
+
+
+# Q2 -> Function to print Name & age
+def greet():
+    print("My Name is Shantanu")
+    print("I am 18")
+
+greet()
+
+# Q3 -> Commnets to explain
+def write(): #Function Defination
+    print("Hello World") #Body Of Funtion that is printting Hello World message On Output
+
+write() #calling function
+
+# Q5 -> Calculator(printing sum)
+
+def add(a,b):
+    print(a+b)
+
+add(5,3)
