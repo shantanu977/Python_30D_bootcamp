63bf88c8dd22d0edceaa82e1b26fa9180fee55ca
Shantanu <shantanuubhe9@gmail.coom>
2025-07-28 18:20:52 +0530

    Updated Day-6

---
+++ day6.py
@@ -0,0 +1,53 @@
+#while loop 1-10 :
+
+# i = 1
+# while(i<=10):
+#     print(i)
+#     i+=1
+    
+
+
+#countdown using number 
+
+# n = int(input("Enter Number : "))
+# while(n>=0):
+#     print(n)
+#     n-=1
+
+
+
+
+# Table of input number
+# n = int(input("Enter Number : "))
+
+# for i in range(1,11):
+#     print(f"{n} * {i} : {n*i}")
+    
+    
+#print 1-20 skip numbers divisible by 3
+
+# i = 1
+
+# while(i<=20) :
+#     if(i%3==0):
+#         i+=1
+#         continue
+#     print(i)
+#     i+=1
+
+
+
+# First 5 Even Numbers from 1 :
+
+# count = 0
+# con = True
+# i=1
+# while(con) :
+#     if(count ==5):
+#         break
+    
+#     if(i%2==0):
+#         count+=1
+#         print(i)
+    
+#     i+=1
